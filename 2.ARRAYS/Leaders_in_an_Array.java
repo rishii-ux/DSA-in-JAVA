abb12356defbb9877aea221f6abf1299f40cde2d
𝗥𝗶𝘀𝗵𝗶 𝗥𝗼𝘆 <rishijsr2006@gmail.com>
2026-08-10 11:13:36 +0530

    Add files via upload

--- 2.ARRAYS/Leaders_in_an_Array.java
+++ 2.ARRAYS/Leaders_in_an_Array.java
@@ -14,8 +14,40 @@ Output:
 
 The last element is always a leader.
 */
-package LEVEL 2 — ARRAYS;
+
+
+import java.util.Scanner;
 
 public class Leaders_in_an_Array {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int size = in.nextInt();
+        int arr[] = new int[size];
+        int brr[] = new int[size];
+        System.out.println("Enter the elements in the array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        
+        int ctr=0;
+        for (int i = 0; i < arr.length; i++) {
+            boolean leader=true;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]<arr[j]){
+                    leader=false;
+                    break;
+                }
+            }
+            if(leader==true){
+                brr[ctr++]=arr[i];
+            }
+        }
+        System.out.println("All leaders in the array are: ");
+        for (int i = 0; i < ctr; i++) {
+            System.out.print(brr[i]+" ");
+        }
+        in.close();
+    }
 }
